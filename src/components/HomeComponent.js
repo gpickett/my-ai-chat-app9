a2a58d5b13133e588271a40cb15661f621040160
gpickett <122489228+gpickett@users.noreply.github.com>
2024-09-19 21:42:17 -0700

    Adding src/components/HomeComponent.js

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,13 @@
+// Home component code here
+import React from 'react';
+
+const HomeComponent = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Welcome to the home page!</p>
+    </div>
+  );
+};
+
+export default HomeComponent;
